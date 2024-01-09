d4831788a45aa8af34b327882a8d5b352f62fd72
ShinSeoY <tjdudtls312@gmail.com>
2024-01-09 11:32:48 +0900

    - java -> groovy - userServiceTest 작성

--- src/main/java/security/dto/LoginDto.java
+++ src/main/java/security/dto/LoginDto.java
@@ -1,12 +1,14 @@
 package security.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
 public class LoginDto {
 
     private String username;
